41e1c8397275eee151bbeee2a2ad64b069368aad
Andy Susanto <officialandysusanto@gmail.com>
2022-02-23 20:42:06 +0700

    master nilai

--- app/Http/Controllers/Master/MasterBobotNilaiController.php
+++ app/Http/Controllers/Master/MasterBobotNilaiController.php
@@ -24,7 +24,7 @@ class MasterBobotNilaiController extends Controller
                         return $row->penyelenggara->nama;
                     })
                     ->addColumn('kategori',function($row){
-                        return $row->kategori->nama_kategori;
+                        return $row->kategori;
                     })
                     ->addColumn('tingkat',function($row){
                         return $row->tingkat->nama;
