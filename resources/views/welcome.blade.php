0bdf2b4b584dd8f901a466b93ac629f321f39052
Andy Susanto <officialandysusanto@gmail.com>
2022-04-14 11:03:15 +0700

    loop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -193,7 +193,7 @@
                     </div>
                 </div>
                 <div class="p-4 col-lg-1 col p-lg-0" style="margin-left:50spx">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/iKVc8szbT1A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/iKVc8szbT1A?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
