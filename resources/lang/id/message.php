0a6cafa415a05b2d0f4dac8acb165ebd9f7b74cd
Andy Susanto <officialandysusanto@gmail.com>
2022-07-07 14:37:08 +0700

    done lang

---
+++ resources/lang/id/message.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    "valid" => [
+        "operator" => "Menunggu Validasi Operator",
+        "wadek" => "Menunggu Validasi Wakil Dekan"
+    ],
+    "success" => [
+        "valid" => "di Validasi",
+        "save"  => "data berhasil di simpan"
+    ],
+    "failed" => [
+        "error" => "terjadi kesalahan, silahkan ulangi kembali",
+        "notValid" => "data tidak diterima"
+    ]
+];
--- resources/views/validasi-rekam-kegiatan/status.blade.php
+++ resources/views/validasi-rekam-kegiatan/status.blade.php
@@ -1,10 +1,10 @@
 @if ($row['validasi'] == '3')
-    <span class="text-black badge badge-warning"><i>Menunggu Validasi Operator</i></span>
+    <span class="text-black badge badge-warning"><i>{{ucword(@lang("message.valid.operator"))}}</i></span>
 @elseif ($row['validasi'] == '1')
-    <span class="text-white badge badge-info"><i>Menunggu Validasi Wakil Dekan</i></span>
+    <span class="text-white badge badge-info"><i>{{ucowrd(@lang("message.valid.wadek"))}}</i></span>
 @elseif ($row['validasi'] == '4')
-    <span class="text-white badge badge-success"><i>di Validasi</i></span>
+    <span class="text-white badge badge-success"><i>{{ucword(@lang("message.success.valid"))}}</i></span>
 @elseif ($row['validasi'] == '2')
-    <span class="text-white badge badge-danger"><i>Tidak di Terima</i></span>
+    <span class="text-white badge badge-danger"><i>{{ucowrd(@lang("message.failed.notValid"))}}</i></span>
     <p class="italic">Pesan : {{$row['pesan']}}</p>
 @endif
