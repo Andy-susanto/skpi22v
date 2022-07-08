b834d8ed213baa09ba9d698f0ed2bb3fa75688f8
Andy Susanto <officialandysusanto@gmail.com>
2022-07-08 19:14:25 +0700

    status kegiatan

--- resources/views/validasi-rekam-kegiatan/status.blade.php
+++ resources/views/validasi-rekam-kegiatan/status.blade.php
@@ -1,10 +1,10 @@
 @if ($row['validasi'] == '3')
-    <span class="text-black badge badge-warning"><i>{{ucwords(@lang("message.valid.operator"))}}</i></span>
+    <span class="text-black badge badge-warning"><i>{{ucwords(__("message.valid.operator"))}}</i></span>
 @elseif ($row['validasi'] == '1')
-    <span class="text-white badge badge-info"><i>{{ucwords(@lang("message.valid.wadek"))}}</i></span>
+    <span class="text-white badge badge-info"><i>{{ucwords(__("message.valid.wadek"))}}</i></span>
 @elseif ($row['validasi'] == '4')
-    <span class="text-white badge badge-success"><i>{{ucwords(@lang("message.success.valid"))}}</i></span>
+    <span class="text-white badge badge-success"><i>{{ucwords(__("message.success.valid"))}}</i></span>
 @elseif ($row['validasi'] == '2')
-    <span class="text-white badge badge-danger"><i>{{ucwords(@lang("message.failed.notValid"))}}</i></span>
+    <span class="text-white badge badge-danger"><i>{{ucwords(__("message.failed.notValid"))}}</i></span>
     <p class="italic">Pesan : {{$row['pesan']}}</p>
 @endif
